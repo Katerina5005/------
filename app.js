971eeb4ade45e6c8b66de1dcfd3615f1495a7b20
Katerina5005 <kat51082@gmail.com>
2022-01-17 19:16:21 +0600

    else if

--- app.js
+++ app.js
@@ -10,8 +10,23 @@ const personalMovieDB = {
 for(let i =0; i < 2; i++) {
     const a = prompt('Один из посмотренных фильмов?', ''),
     b = prompt('На сколько оцените его?', '');
+
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
     
-    personalMovieDB.movies[a] = b;
+if (personalMovieDB.count < 10) {
+    console.log('Мало');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('класс');
+} else if(personalMovieDB.count >= 30) {
+    console.log('киноман');
+} else {
+    console.log('ошибка');
+}
+
 }
 
 console.log(personalMovieDB);
